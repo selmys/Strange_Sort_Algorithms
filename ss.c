7f4a5790bff8381f0373692dce42c92ea8c8c9f3
John Selmys <selmys@lotuspond.ca>
2021-08-24 09:50:40 -0400

    Added Examples

---
+++ ss.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE 11
+
+int main() {
+	int x[SIZE]={12,3,16,77,3,21,19,91,67,-23};
+	int notsorted=1,i,n1,n2;
+	long count=0;
+	while(notsorted) {
+		count++;
+		n1=rand()%SIZE;
+		n2=rand()%SIZE;
+		int t=x[n1];
+		x[n1]=x[n2];
+		x[n2]=t;
+		notsorted=0;
+		for(i=0;i<SIZE-1;i++) 
+			if(x[i]>x[i+1]) {
+				notsorted=1;
+				break;
+			} 
+	}
+	printf("x is sorted in %ld swaps\n",count);
+	for(i=0;i<SIZE;i++)
+		printf("%d ",x[i]);
+	return 0;
+}
